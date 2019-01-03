6f9f84286ff1700a0e778bde90439e014b4104ac
Dhruv Ramani <dhruvramani@Dhruvs-MacBook-Air.local>
2019-01-03 09:36:49 +0530

    dataset test

--- src/dataset.py
+++ src/dataset.py
@@ -56,6 +56,7 @@ class OffenseEval(Dataset):
         contents['SUBC'] = SUBC.index(contentstr[4])
         contents['embeddings'] = np.asarray([self.glove.get(word, self.glove['unk']) for word in  contents["instance"].split(" ")])
         # TODO:  concatenate end of line word embedding, then :
+        print(contents["embeddings"].shape[0])
         concat = np.zeros((_MAX_LEN - contents["embeddings"].shape[0], _EMB_DIM))
         contents["embeddings"] = np.concatenate((contents["embeddings"], concat))
         return contents
@@ -77,4 +78,4 @@ if __name__ == '__main__':
     dataloader = iter(dataloader)
     for i in range(0, len(dataloader)):
         embeddings, suba = next(dataloader)
-        print(suba)
+        #print(suba.shape)
